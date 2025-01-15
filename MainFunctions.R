be4ade733f3091786d5d4209fb2af16d3ab9ba2b
yijunchenguw <chengxb@uw.edu>
2025-01-14 22:45:08 -0800

    first commit

---
+++ MainFunctions.R
@@ -0,0 +1,135 @@
+library(torch)
+library(lavaan)
+library(psych)
+library(sdpt3r)
+library(fungible)
+library(misty)
+library(MASS)  
+
+source("approaches.R")
+source("ModelDefine.R")
+numofItems <- 12
+setwd(path)
+n_factors <- 2
+df <- 53
+# Define lambda structure as a binary matrix indicating fixed vs. free parameters
+lambda_structure <- matrix(c(
+  1, 0,
+  1, 0,
+  1, 0,
+  1, 0,
+  1, 0,
+  1, 0,
+  0, 1,
+  0, 1,
+  0, 1,
+  0, 1,
+  0, 1,
+  0, 1
+), nrow = 12, byrow = TRUE)
+lambda_mask <- torch_tensor(lambda_structure)
+for(size in c(100, 200,500))
+{
+  for(loading in c(9))
+  {
+    nameofmodel <- get(paste0("pop.mod",".", loading, sep = ""))
+    mod_constraint <- get("mod_constraint")
+    cat(nameofmodel, "\n")
+    se_1 <- c()
+    se_2 <- c()
+    se_3 <- c()
+    se_4 <- c()
+    se_5 <- c()
+    upper_1 <- c()
+    upper_2 <- c()
+    upper_3 <- c()
+    upper_4 <- c()
+    upper_5 <- c()
+    lower_1 <- c()
+    lower_2 <- c()
+    lower_3 <- c()
+    lower_4 <- c()
+    lower_5 <- c()
+    result_loading_1 <- c()
+    result_loading_2 <- c()
+    result_loading_3 <- c()
+    result_loading_4 <- c()
+    result_loading_5 <- c()
+    heywood <- 0
+    for (i in c(1:500)) {
+    cat("loading:",loading,";size:", size,";i:",i, "\n")
+    set.seed(i+100)
+    datb <- simulateData(nameofmodel, sample.nobs=size)-1
+    fit_method1 <- lavaan::cfa(mod_no_constraint, data=datb)
+    fit_method2 <- lavaan::cfa(mod_constraint, data=datb)
+    result_lavaan_1 <- summary(fit_method1, fit.measures=TRUE, rsquare=TRUE, standardized=TRUE)
+    ci_95_upper <- result_lavaan_1$pe$est[1:numofItems] + 1.96*result_lavaan_1$pe$se[1:12]
+    upper_1 <- rbind(upper_1, ci_95_upper)
+    ci_95_lower <- result_lavaan_1$pe$est[1:numofItems] - 1.96*result_lavaan_1$pe$se[1:12]
+    lower_1 <- rbind(lower_1, ci_95_lower)
+    
+    result_lavaan_2 <- summary(fit_method2, fit.measures=TRUE, rsquare=TRUE, standardized=TRUE)
+    
+    #rmsea_lavaan_1 <- rbind(rmsea_lavaan,result_lavaan$fit["rmsea"] )
+    result_loading_1 <<- rbind(result_loading_1, result_lavaan_1$pe$est[1:numofItems])
+    result_loading_2 <<- rbind(result_loading_2, result_lavaan_2$pe$est[1:numofItems])
+       
+    ci_95_upper <- result_lavaan_2$pe$est[1:numofItems] + 1.96*result_lavaan_2$pe$se[1:12]
+    upper_2 <- rbind(upper_2, ci_95_upper)
+    
+    ci_95_lower <- result_lavaan_2$pe$est[1:numofItems] - 1.96*result_lavaan_2$pe$se[1:12]
+    lower_2 <- rbind(lower_2, ci_95_lower)
+    if(any(result_lavaan_1$pe$est[(numofItems+4):(numofItems+3+12)]<0))
+    {
+      heywood <- heywood + 1
+      print(result_lavaan_1$pe$est[(numofItems+4):(numofItems+3+12)])
+    }
+    try({
+       result3 <- mle_method(datb, 3, n = size)
+       result4 <- mle_method(datb, 4, n = size)
+       result5 <- mle_method(datb, 5, n = size)
+    },
+    next )
+    
+    result_loading_3 <- rbind(result_loading_3,rowSums(as.matrix(result3$lambda*lambda_mask)))
+    result_loading_4 <- rbind(result_loading_4,rowSums(as.matrix(result4$lambda*lambda_mask)))
+    result_loading_5 <- rbind(result_loading_5,rowSums(as.matrix(result5$lambda*lambda_mask)))
+    
+    upper_3 <- rbind(upper_3,result3$upper)
+    upper_4 <- rbind(upper_4,result4$upper)
+    upper_5 <- rbind(upper_5,result5$upper)
+    lower_3 <- rbind(lower_3,result3$lower)
+    lower_4 <- rbind(lower_4,result4$lower)
+    lower_5 <- rbind(lower_5,result5$lower)
+    }
+    true_loading <- loading/10
+    ###### find the percentage of true loading inside the 95% confidence interval ------------
+    ci_95 <- rbind(
+    percentage95(upper_1, lower_1, true_loading),
+    percentage95(upper_2, lower_2, true_loading),
+    percentage95(upper_3, lower_3, true_loading),
+    percentage95(upper_4, lower_4, true_loading),
+    percentage95(upper_5, lower_5, true_loading)
+    )
+    rmse_result <- rbind(
+      rmse(c(result_loading_1), true_loading),
+      rmse(c(result_loading_2), true_loading),
+      rmse(c(result_loading_3), true_loading),
+      rmse(c(result_loading_4), true_loading),
+      rmse(c(result_loading_5), true_loading)
+    )
+    result <- cbind(ci_95,rmse_result, heywood)
+    write.csv(result, paste0(loading,"-size",size,"-result",".csv"))
+    
+    loading1 <- rmse_condition(result_loading_1, true_loading)
+    loading2 <- rmse_condition(result_loading_2, true_loading)
+    loading3 <- rmse_condition(result_loading_3, true_loading)
+    loading4 <- rmse_condition(result_loading_4, true_loading)
+    loading5 <- rmse_condition(result_loading_5, true_loading)
+    approach=c(rep("Method1",length(loading1)),rep("Method2",length(loading2)),rep("Method3",length(loading3)),rep("Method4",length(loading4)),rep("Method5",length(loading5)))
+    data_agg=data.frame(value=c(c(loading1),c(loading2),c(loading3),c(loading4),c(loading5)),  approach = approach)
+    write.csv(data_agg, paste0(loading,"-size",size,"-loading",".csv"))
+  }
+}
+
+
---
+++ approaches.R
@@ -0,0 +1,249 @@
+
+############# initialize -----
+inverse_sigmoid <- function(x)
+{
+  -log(2/(x + 1) - 1)
+  #1/(1+exp(-x))
+  #x
+  #tanh(x)
+}
+cfa_log_likelihood_test <- function(lambda, theta_delta, phi, sample_cov, n) {
+  #theta_delta_diag <- torch_diag(theta_delta)  # Ensure theta_delta is diagonal
+  theta_delta_diag <- torch_diag(theta_delta) 
+  lambda <- lambda*lambda_mask
+  #phi_diag <- torch_eye(n_factors)
+  phi_diag <- phi.matrix(phi)$requires_grad_(T)
+  #print(phi_diag)
+  implied_cov <- torch_matmul(torch_matmul(lambda, phi_diag),(lambda$t())) + theta_delta_diag
+  #regularization_constant<- 1e-20
+  #implied_cov <- implied_cov + diag(regularization_constant, nrow(implied_cov))  # Regularize to avoid singularity
+  # Log determinants
+  logdet_implied <- torch_logdet(implied_cov)
+  logdet_sample <- torch_logdet(sample_cov)
+  n_variables <- nrow(sample_cov)
+  # Inverse of implied covariance matrix
+  inv_implied <- torch_inverse(implied_cov )
+  log_likelihood <- -0.5 *n* (n_variables*log(2*pi) + logdet_implied + sum(torch_diag(torch_matmul(sample_cov, inv_implied))))       
+  #cat(as.numeric(inv_implied[1,1]), ",", as.numeric(log_likelihood), "\n")
+  return(-log_likelihood)  # Negative log-likelihood for minimization
+}
+# CFA Log-likelihood function with improved stability
+cfa_log_likelihood_map_exp <- function(lambda, theta_delta, phi, sample_cov, n) {
+  #theta_delta_diag <- torch_diag(theta_delta)  # Ensure theta_delta is diagonal
+  theta_delta_diag <- torch_diag(exp(theta_delta)) 
+  phi_diag <- phi.matrix(phi)$requires_grad_(T)
+  lambda <- (lambda)*lambda_mask
+
+  implied_cov <- torch_matmul(torch_matmul(lambda, phi_diag),(lambda$t())) + theta_delta_diag
+  # Log determinants
+  logdet_implied <- torch_logdet(implied_cov)
+  logdet_sample <- torch_logdet(sample_cov)
+  n_variables <- nrow(sample_cov)
+  # Inverse of implied covariance matrix
+  inv_implied <- torch_inverse(implied_cov )
+  penalty_weight <- 1
+  log_likelihood <- -0.5 *n* (n_variables*log(2*pi) + logdet_implied + sum(torch_diag(torch_matmul(sample_cov, inv_implied)))) - torch_tensor((lambda^2)/(0.2^2))$sum()           
+  #cat(as.numeric(inv_implied[1,1]), ",", as.numeric(log_likelihood), "\n")
+  return(-log_likelihood)  # Negative log-likelihood for minimization
+}
+
+cfa_log_likelihood_sigmoid_exp <- function(lambda, theta_delta, phi, sample_cov, n) {
+  #theta_delta_diag <- torch_diag(theta_delta)  # Ensure theta_delta is diagonal
+  theta_delta_diag <- torch_diag(exp(theta_delta)) 
+  phi_diag <- phi.matrix(phi)$requires_grad_(T)
+  lambda <- sigmoid(lambda)*lambda_mask
+  
+  implied_cov <- torch_matmul(torch_matmul(lambda, phi_diag),(lambda$t())) + theta_delta_diag
+  regularization_constant<- 1e-20
+  implied_cov <- implied_cov + diag(regularization_constant, nrow(implied_cov))  # Regularize to avoid singularity
+  n_variables <- nrow(sample_cov)
+  # Log determinants
+  logdet_implied <- torch_logdet(implied_cov)
+  logdet_sample <- torch_logdet(sample_cov)
+  
+  # Inverse of implied covariance matrix
+  inv_implied <- torch_inverse(implied_cov )
+  log_likelihood <- -0.5 *n* (n_variables*log(2*pi) + logdet_implied + sum(torch_diag(torch_matmul(sample_cov, inv_implied))))           
+  #cat(as.numeric(inv_implied[1,1]), ",", as.numeric(log_likelihood), "\n")
+  return(-log_likelihood)  # Negative log-likelihood for minimization
+}
+
+
+cfa_log_likelihood_exp <- function(lambda, theta_delta, phi, sample_cov, n) {
+  #theta_delta_diag <- torch_diag(theta_delta)  # Ensure theta_delta is diagonal
+  theta_delta_diag <- torch_diag(exp(theta_delta)) 
+  lambda <- lambda*lambda_mask
+  
+  phi_diag <- phi.matrix(phi)$requires_grad_(T) 
+  implied_cov <- torch_matmul(torch_matmul(lambda, phi_diag),(lambda$t())) + theta_delta_diag
+  regularization_constant<- 1e-20
+  implied_cov <- implied_cov + diag(regularization_constant, nrow(implied_cov))  # Regularize to avoid singularity
+  
+  # Log determinants
+  logdet_implied <- torch_logdet(implied_cov)
+  logdet_sample <- torch_logdet(sample_cov)
+  n_variables <- nrow(sample_cov)
+  # Inverse of implied covariance matrix
+  inv_implied <- torch_inverse(implied_cov )
+  penalty_weight <- 1
+  log_likelihood <- -0.5 *n* (n_variables*log(2*pi) + logdet_implied + sum(torch_diag(torch_matmul(sample_cov, inv_implied))))           
+  #cat(as.numeric(inv_implied[1,1]), ",", as.numeric(log_likelihood), "\n")
+  return(-log_likelihood)  # Negative log-likelihood for minimization
+}
+
+sigmoid <- function(x)
+{
+  -1 + 2/(1+exp(-x))
+  #1/(1+exp(-x))
+  #x
+  #tanh(x)
+}
+exp_input <- function(x)
+{
+  #-1 + 2/(1+exp(-x))
+  #1/(1+exp(-x))
+  exp(x)
+  #tanh(x)
+}
+delta_method <- function(x){
+  2*exp(-x)/(1+exp(-x))^2
+}
+vec <- function(x) {
+  torch_cat(lapply(x, function(x) {
+    x$view(-1)
+  }))
+}
+bic <- function(logl, npar, N)
+{
+  -2*logl + npar*log(N)
+}
+
+phi.matrix <- function(v)
+{
+  K <- length(v)
+  if(K == 1) K = 2
+  z <- v$tanh()
+  Sigma1.L.mask <- torch_tensor(lower.tri(matrix(0, K, K)))$bool()
+  Sigma1.L <- torch_eye(K)$masked_scatter(Sigma1.L.mask, z) * torch_cat(c(torch_ones(K, 1), (1 - torch_zeros(K, K)$masked_scatter(Sigma1.L.mask, z)[, 1:-2]$square())$cumprod(2)$sqrt()), 2)
+  torch_matmul(Sigma1.L,  Sigma1.L$t())
+}
+percentage95 <- function(upper, lower, true){
+  i <- 1
+  numofCI <- 0
+  while (i<length(upper)+1) {
+    
+    if(!is.na(upper[i]) && !is.na(lower[i]) && as.numeric(upper[i]) > true && as.numeric(lower[i]) < true)
+    {
+      numofCI = numofCI + 1
+    }
+    i <- i + 1
+  }
+  numofCI/length(!is.na(upper))
+}
+rmse <- function(estimation, true)
+{
+  len = length(estimation)
+  bias = 0
+  for(i in c(1:len))
+  {
+    bias = bias + (estimation[i] - true)^2 
+  }
+  sqrt(bias/len)
+}
+rmse_condition <- function(loading, true)
+{
+  loading_rmse <- c()
+  len <- nrow(loading)
+  for(i in c(1:len))
+  {
+    #print(loading[i,])
+    temp <- rmse(loading[i,],true)
+    loading_rmse <- c(loading_rmse,temp )
+  }
+  loading_rmse
+}
+
+mle_method <- function(datb, method = 3, tolerance = 1e-4, n){
+# Load your sample covariance data (assume 'datb' is loaded)
+sample_cov <- cov(datb)  # Base covariance matrix from your data
+sample_cov_tensor <- torch_tensor(sample_cov)  # Convert to torch tensor
+# Number of observed variables and latent factors
+n_variables <- nrow(sample_cov)
+result <- c()
+# Initialize lambda with specific values at non-zero positions defined by lambda_structure
+initial_lambda_values <- matrix(0, nrow = n_variables, ncol = n_factors)
+ # Set non-zero loadings to an initial value
+# Initializing theta_delta with the diagonal of sample covariance
+  if(method == 3) {
+  initial_lambda_values[lambda_structure == 1] <- 0.1 
+  cfa_function <- cfa_log_likelihood_exp
+  }else if(method == 4) {
+    initial_lambda_values[lambda_structure == 1] <- inverse_sigmoid(0.1)
+    cfa_function <- cfa_log_likelihood_sigmoid_exp
+  }else if(method == 5) {
+    initial_lambda_values[lambda_structure == 1] <- 0.1
+    cfa_function <- cfa_log_likelihood_map_exp
+  }
+  
+lambda <- torch_tensor(initial_lambda_values, requires_grad = TRUE)
+theta_delta <- torch_tensor(log(diag(sample_cov)), requires_grad = TRUE)
+phi <- torch_tensor(0.3, requires_grad = TRUE)
+
+# Adam optimizer with a smaller learning rate for stability
+optimizer <- optim_adam(params = list(lambda, theta_delta, phi), lr = 0.1)
+
+# Optimization loop with more epochs
+num_epochs <- 100000
+
+last_loss <- NULL
+
+for (epoch in 1:num_epochs) {
+  optimizer$zero_grad()  # Zero out gradients
+
+  loss <- cfa_function(lambda, theta_delta, phi, sample_cov_tensor, n)  # Compute loss
+  loss$backward()  # Backpropagation
+  
+  # Apply gradient mask to enforce structure, keeping gradients only where lambda_structure == 1
+  with_no_grad({
+    lambda$grad <- lambda$grad * lambda_mask  # Zero out gradients where lambda_structure == 0
+  })
+  
+  optimizer$step()  # Update parameters
+  
+  # Early stopping condition
+  if (!is.null(last_loss) && abs(last_loss - loss$item()) < tolerance) {
+    cat("Early stopping at epoch:", epoch, "with loss:", loss$item(), "\n")
+    loss <- cfa_function(lambda, theta_delta, phi, sample_cov_tensor, n)  # Compute loss
+    loss$backward(retain_graph = TRUE)
+    
+    # Step 1: Compute first-order gradients with autograd_grad
+    first_derivative_lambda <- autograd_grad(loss, inputs = list(lambda, theta_delta, phi), create_graph = TRUE)
+    
+    result <- lapply(vec(first_derivative_lambda)$unbind(),function(l) {
+      vec(autograd_grad(l, list(lambda, theta_delta, phi), retain_graph = T))
+    })
+    var_x <- c(as.array(torch_diag(torch_stack(result)$pinverse()))[1:(2*n_variables)])
+    var <- c(var_x[c(1,3,5,7,9,11,14,16,18,20,22,24)])
+    ci_95_upper <- (lambda*lambda_structure)$sum(2) + 1.96*sqrt(var)
+    ci_95_lower <-  (lambda*lambda_structure)$sum(2) - 1.96*sqrt(var)
+    if(method == 4) 
+    {
+      lambda <- sigmoid(lambda)
+      var <- c(t(as.array(delta_method(lambda)^2)))*var
+      ci_95_upper <- sigmoid(ci_95_upper)
+      ci_95_lower <- sigmoid(ci_95_lower)
+      
+    }
+     se <- sqrt(var)
+
+    
+    result <- list(lambda = as.matrix(lambda), se = as.matrix(se),  upper = as.matrix(ci_95_upper), lower = as.matrix(ci_95_lower),theta = as.matrix(exp(theta_delta)), phi = as.matrix(phi.matrix(phi)), ll = as.numeric(loss$item()))
+    result
+    break
+  }
+  
+  
+  last_loss <- loss$item()
+  }
+  return(result)
+}
